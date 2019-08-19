d2cc1ed85af38b2ccbad04cfc53b69a08cca52b8
ethanpasta <ethan.mayer9@gmail.com>
2019-08-19 22:48:56 +0000

    First task

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Script sets up your web server for the deployment of web_static
+apt-get -y update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
+mkdir -p /data/
+mkdir -p /data/web_static/
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo """
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+""" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '/root /var/www/html;/a location hbnb_static {\n    alias /data/web_static/current/;\n}' /etc/nginx/sites-available/default
+service nginx restart
+exit 0
