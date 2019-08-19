f2e8a69a23a2c7a89beadf8d278e9e253749ec19
ethanpasta <ethan.mayer9@gmail.com>
2019-08-19 23:10:02 +0000

    Add sed fixes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,17 +8,15 @@ mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo """
-<html>
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>
-""" > /data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '/root /var/www/html;/a location hbnb_static {\n    alias /data/web_static/current/;\n}' /etc/nginx/sites-available/default
+sed -i '/root \/var\/www\/html;/a location \/hbnb_static\/ { alias \/data\/web_static\/current\/;}' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
