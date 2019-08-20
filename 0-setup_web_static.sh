6d35ecd8463e34152bee29f71b44222db86429f8
ethanpasta <ethan.mayer9@gmail.com>
2019-08-20 04:09:43 +0000

    Remove slash

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,6 @@ apt-get -y install nginx
 ufw allow 'Nginx HTTP'
 mkdir -p /data/
 mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 echo "<html>
@@ -16,7 +15,7 @@ echo "<html>
   </body>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+chown -R ubuntu:ubuntu /data
 sed -i '/root \/var\/www\/html;/a location \/hbnb_static\/ { alias \/data\/web_static\/current\/;}' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
