df27536fb7a3027a8543fe60bfd18b424633618c
ethanpasta <ethan.mayer9@gmail.com>
2019-08-20 05:07:09 +0000

    Remove \ from ln command

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,7 +14,7 @@ echo "<html>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
 sed -i '/root \/var\/www\/html;/a location \/hbnb_static\/ { alias \/data\/web_static\/current\/;}' /etc/nginx/sites-available/default
 service nginx restart
