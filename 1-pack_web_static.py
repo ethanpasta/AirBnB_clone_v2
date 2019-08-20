061148704365db667de1eca3424c917fb75b6dd4
ethanpasta <ethan.mayer9@gmail.com>
2019-08-20 00:02:52 +0000

    Task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """ Function generates a .tgz archive from the
+        contents of the 'web_static' folder
+    """
+    local("mkdir -p versions")
+    now = datetime.datetime.now()
+    filename = "versions/web_static_{}.tgz".format(
+        now.strftime("%Y%m%dT%H%M%S"))
+    result = local("tar -czvf {} web_static".format(filename))
+    if result.failed:
+        return None
+    return result
