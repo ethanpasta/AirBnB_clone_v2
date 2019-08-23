c2b16448123de749648c93514583184240db32ce
ethanpasta <ethan.mayer9@gmail.com>
2019-08-23 17:45:46 +0000

    Add do_pack function

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-from fabric.api import env, run, put, sudo
+from fabric.api import env, run, put, sudo, local
 import os
+import datetime
 
 
 env.hosts = ['35.196.108.226', '34.74.255.195']
@@ -9,10 +10,21 @@ env.user = "ubuntu"
 env.key_filename = "~/.ssh/holberton"
 
 
-def deploy(archive_path):
+def do_pack():
+    """ Function generates a .tgz file """
+    local("mkdir -p versions")
+    now = datetime.datetime.now()
+    filename = "versions/web_static_{}.tgz".format(
+        now.strftime("%Y%m%d%H%M%S"))
+    result = local("tar -czvf {} web_static".format(filename))
+    if result.failed:
+        return None
+    return result
+
+
+def do_deploy(archive_path):
     """ Function distributes an archive to web servers """
     if not os.path.exists(archive_path):
-        print("hi")
         return False
     filename = os.path.basename(archive_path)
     name = filename.split(".")[0]
