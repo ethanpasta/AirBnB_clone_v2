bfcc47729dbba61420bc733c002b3436ff224c5a
ethanpasta <40837133+ethanpasta@users.noreply.github.com>
2019-08-23 09:36:54 -0700

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,6 +16,6 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed -i '/root \/var\/www\/html;/a location \/hbnb_static\/ { alias \/data\/web_static\/current\/;}' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
 exit 0
