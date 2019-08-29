c14b81f92a957efc64a386d38c8088aa2130bfef
ethanpasta <ethan.mayer9@gmail.com>
2019-08-29 18:46:30 +0000

    Task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Module for simple flask application - Task 2 """
+
+from web_flask import app
+
+
+@app.route('/')
+def index():
+    """ Function returns string with flask """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def index_hbnb():
+    """ Function returns a string for /hbnb route """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_route(text):
+    """ Function returns a string containing a variable """
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
