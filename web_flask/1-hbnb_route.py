45ca61316e02b84e564f3bef93d226cf9441b8c5
ethanpasta <ethan.mayer9@gmail.com>
2019-08-29 18:41:00 +0000

    Task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module for simple flask application - Task 1 """
+
+from web_flask import app
+
+
+@app.route('/')
+def index():
+    """ Function returns string with flask """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def index_hbnb():
+    """ Function returns a string for /hbnb route """
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
