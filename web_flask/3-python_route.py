0b8fd62c6183fcefcd5595b3fc909bc408aae251
ethanpasta <ethan.mayer9@gmail.com>
2019-08-29 19:10:07 +0000

    Remove extra paramater

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,7 +24,7 @@ def c_route(text):
 
 @app.route('/python', defaults={'text': "is cool"}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text="is cool"):
+def python_route(text):
     """ Function returns a string containing a variable """
     return "Python {}".format(text.replace('_', ' '))
 
