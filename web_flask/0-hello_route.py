0bc4aceb3ecf2f3f1d7a834376a72b9957db94db
ethanpasta <ethan.mayer9@gmail.com>
2019-08-29 19:08:30 +0000

    Add  strict_slashes=False to routes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,11 +4,10 @@
 from web_flask import app
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
     """ Function returns string with flask """
     return "Hello HBNB!"
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,17 +4,16 @@
 from web_flask import app
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
     """ Function returns string with flask """
     return "Hello HBNB!"
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def index_hbnb():
     """ Function returns a string for /hbnb route """
     return "HBNB"
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
