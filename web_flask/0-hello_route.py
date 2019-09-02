919d706910eee2e5e8cac1776652182e331098c2
ethanpasta <ethan.mayer9@gmail.com>
2019-09-02 18:17:16 +0000

    Add flask import

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Module starts a flask web application """
 
-from web_flask import app
+from flask import Flask
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
