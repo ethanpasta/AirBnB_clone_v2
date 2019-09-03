29a7cc1cd91f25b5e4cccc2b2fe4a2b9f99c3090
ethanpasta <ethan.mayer9@gmail.com>
2019-09-03 02:36:16 +0000

    Add attributes for __init__

--- models/base_model.py
+++ models/base_model.py
@@ -37,6 +37,10 @@ class BaseModel:
                     setattr(self, key, value)
             if self.id is None:
                 self.id = str(uuid.uuid4())
+            if self.created_at is None:
+                self.created_at = datetime.now()
+            if self.updated_at is None:
+                self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
